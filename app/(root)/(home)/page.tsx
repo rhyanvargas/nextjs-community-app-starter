564949b8231d61f2af9d830b18ac5bebe7846604
Rhyan Vargas <rhyanv1@gmail.com>
2023-09-29 14:15:48 -0700

    add userButton auth.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
 	return (
-		<main className="flex min-h-screen w-full">
-			<h1>HOME PAGE</h1>
-		</main>
+		<div>
+			<UserButton afterSignOutUrl="/" />
+		</div>
 	);
 }
