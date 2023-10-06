b320d0d82fb3a3f75d4fcbba9484563cc3cec133
Rhyan Vargas <rhyanv1@gmail.com>
2023-10-05 23:47:45 -0700

    add radix, next-themes, and update providers

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,52 +1,12 @@
 "use client";
 
-import React, { createContext, useContext, useEffect, useState } from "react";
-
-export interface ThemeContextType {
-  mode: string;
-  setMode: (mode: string) => void;
-}
-export const ThemeContext = createContext<ThemeContextType | undefined>(
-  undefined
-);
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export default function ThemeProvider({
   children,
-}: {
-  children: React.ReactNode;
-}) {
-  const [mode, setMode] = useState("dark");
-  const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
-      setMode("dark");
-      document.documentElement.classList.add("dark");
-    }
-  };
-  useEffect(() => {
-    handleThemeChange();
-  }, [mode]);
-
-  return (
-    <ThemeContext.Provider
-      value={{
-        mode,
-        setMode,
-      }}
-    >
-      {children}
-    </ThemeContext.Provider>
-  );
-}
-
-export function useTheme() {
-  const context = useContext(ThemeContext);
-
-  if (context === undefined) {
-    throw new Error("useTheme must be used within a ThemeProvider");
-  }
-
-  return context;
+  ...props
+}: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
