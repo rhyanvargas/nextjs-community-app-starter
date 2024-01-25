d4f4516b8e3f78eae40c08f866c79510f572c2b1
Rhyguy <rhyan@readyweb3.io>
2024-01-24 22:19:07 -0800

    adjust config and stylsheets

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	transpilePackages: ["lucide-react"], // Enable Dynamic import of lucide-react
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "i.pravatar.cc",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
